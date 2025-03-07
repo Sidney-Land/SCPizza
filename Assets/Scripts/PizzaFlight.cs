b3857462cca98236211ae3303c5ba5cd7d06a339
Sidney-Land <136678741+Sidney-Land@users.noreply.github.com>
2025-03-06 19:04:27 -0500

    Camera Following and Added Pizza Platter
    + Camera now follows Player using Unity's Cinemachine package.
    
    + Added a platter to the Player where pizza flies from, which the Player's sprite will eventually hold ready made pizzas to throw.
    
    * Added comments to pre-existing scripts

--- Assets/Scripts/PizzaFlight.cs
+++ Assets/Scripts/PizzaFlight.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PizzaFlight : MonoBehaviour
 {
+    // Speed pizza flies forward
     public float flightSpeed;
 
     // Start is called before the first frame update
@@ -15,18 +16,17 @@ public class PizzaFlight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Fly forward at flightSpeed, Vector2.up is where the pizza points
         transform.Translate(flightSpeed * Time.deltaTime * Vector2.up);
-
     }
 
     // OnTriggerEnter2D is called when the Collider2D other enters the trigger (2D physics only)
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Pizza has been sent to a patron
         if (collision.CompareTag("Characters"))
         {
             Destroy(gameObject);
         }
     }
-
-
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // integer used for the left mouse button
     private const int LEFT_MOUSE = 0;
 
+    // Magnitude of Player's velocity in units per second
     public float moveSpeed;
 
+    // Direction of Player's velocity
     private Vector2 moveDirection = new Vector2();
 
+    // Reference to rigidbody for movement
     public Rigidbody2D rb;
 
+    public Transform pizzaPlatter;
+
+    // Pizza to be thrown
     public GameObject cheesePizza; 
 
     // Start is called before the first frame update
@@ -23,27 +30,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Upon click
         if (Input.GetMouseButtonDown(LEFT_MOUSE))
         {
+            // Get where Player clicked in world coordinates
             Vector2 tossPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+            // Get the direction pizza will fly towards to determine its rotation
             Vector2 tossDirection = (tossPos - (Vector2)transform.position).normalized;
-
             Quaternion tossRotation = Quaternion.Euler(0f, 0f, Vector2.SignedAngle(Vector2.up, tossDirection));
 
-            Instantiate(cheesePizza, tossPos, tossRotation);
+            // Create pizza
+            Instantiate(cheesePizza, pizzaPlatter.position, tossRotation);
         }
     }
 
     // This function is called every fixed framerate frame, if the MonoBehaviour is enabled
     private void FixedUpdate()
     {
+        // Get movement input
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         moveDirection = new Vector2(moveX, moveY).normalized;
 
+        // Change Player's velocity
         rb.velocity = moveSpeed * moveDirection;
     }
-
-
 }
